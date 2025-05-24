88bc77661361f5633b74cfaa1d775608a9f6df93
TA2k <tombox2020@gmail.com>
2025-05-24 10:44:03 +0200

    fix typo

--- main.js
+++ main.js
@@ -813,7 +813,7 @@ class Frigate extends utils.Adapter {
         .replace(/{{zones}}/g, message.zones || '');
       if (message.clipm3u8) {
         //add clipm3u8 to messageText as a href link master.m3u8 and add clipUrl as a href mp4
-        messageText += `<a href="${message.clipm3u8}">Clip Safari</a><br><a href="${message.clipUrl}">Clip MP4</a>`;
+        messageText += `${message.source}: <a href="${message.clipm3u8}">Clip Safari</a><br><a href="${message.clipUrl}">Clip MP4</a>`;
 
         fileName = '';
         type = 'typing';
